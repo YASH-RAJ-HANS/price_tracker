ef3ec94366cafe48d798ccf18a8392cb67b5495c
YASH-RAJ-HANS <yashhans479@gmail.com>
2023-10-17 17:57:52 +0530

    new pro

---
+++ parser.js
@@ -0,0 +1,66 @@
+
+const nightmare = require("nightmare")();
+require('dotenv').config();
+const cron = require('node-cron');
+var price ;
+const nodemailer = require('nodemailer');
+const emailUser = process.env.EMAIL_USER;
+const emailPass = process.env.EMAIL_PASS;
+async function checkPrice() {
+  try {
+    const priceString = await nightmare
+      .goto(
+        "https://www.amazon.in/realme-Feather-Segment-Charging-Slimmest/dp/B0C45N5VPT/"
+      )
+      .wait("#corePriceDisplay_desktop_feature_div")
+      .evaluate(
+        () =>
+          document.getElementById("corePriceDisplay_desktop_feature_div")
+            .innerText
+      )
+      .end();
+    console.log("priceString->", priceString);
+    const pattern = /₹(\d{1,3}(?:,\d{3})*)(?![\d,])/;
+    const matches = priceString.match(pattern);
+    if (matches && matches.length > 1) {
+      const mrp = matches[1];
+      
+      var numberWithoutCommas = parseFloat(mrp.replace(/,/g, ''));
+      price = numberWithoutCommas;
+      console.log(numberWithoutCommas);
+    } else {
+      console.log("MRP not found in the input string.");
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: emailUser,
+      pass: emailPass,
+  },
+});
+
+const mailOptions = {
+  from: 'yashhans479@gmail.com',
+  to: 'yashrajhans4567@gmail.com',
+  subject: 'price per minute',
+  text: '{$price}',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+      console.error('Error:', error.message);
+  } else {
+      console.log('Email sent:', info.response);
+  }
+});
+
+cron.schedule('* * * * *', () => {
+  checkPrice();
+  console.log("yash");
+}, {
+  timezone: 'Asia/Kolkata' 
+});
